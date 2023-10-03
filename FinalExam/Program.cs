af0e5b1135dd1b920fbe4a05f49e7be785fbc927
Feodor <mr.fedor.kiselev@gmail.com>
2023-10-03 19:23:36 +0300

    first array added

--- FinalExam/Program.cs
+++ FinalExam/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+System.Console.WriteLine("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] GetArray(int n)
+{
+    string[] array = new string[n];
+    System.Console.WriteLine("Введите массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+string[] firstArray = GetArray(size);
